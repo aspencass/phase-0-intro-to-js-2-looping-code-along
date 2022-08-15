f814dc4e338ff5b1f0e8e966461bb2d923909dcf
aspencass <caspenw1@gmail.com>
2022-08-15 16:08:31 -0600

    commit tree b27709672daaab3966b0b3be804bc27f71ab774b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(thanksArray, adjective) {
+  let newArray =[]
+  for (let name of thanksArray) {
+    newArray.push(`Thank you, ${name}, for the wonderful ${adjective} gift!`)
+  }
+  return newArray
+      
+
+
+  // newArray.push(`Thank you, ${thanksArray[0]}, for the wonderful ${adjective} gift!`)
+  // newArray.push(`Thank you, ${thanksArray[1]}, for the wonderful ${adjective} gift!`)
+  // newArray.push(`Thank you, ${thanksArray[2]}, for the wonderful ${adjective} gift!`)
+  // return newArray
+}
+
+   
+function countDown(counter) {
+  // let counter = 10;
+    while (counter >= 0) {
+      console.log(counter);
+      counter--;
+      };
+}
+console.log(countDown(10))
